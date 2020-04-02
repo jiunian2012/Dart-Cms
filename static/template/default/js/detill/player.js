3e10f735faac4c8993a2fc353b5a7d27f5823873
tump <abcd498936590@gmail.com>
2020-04-02 12:33:06 +0800

    修改模板

--- static/template/default/js/detill/player.js
+++ static/template/default/js/detill/player.js
@@ -64,7 +64,7 @@
 		var listIndex = query.hasOwnProperty('index') && tabSource[tabIndex].list[query['index']] ? Number(query['index']) : 0;
 		var frameH = ($(window).width() < 990 ? 300 : 500) + 'px';
 
-		playerBox.css({height: frameH, backgroundColor: '#000'});
+		// playerBox.css({height: frameH, backgroundColor: '#000'});
 
 		// 是否禁用
 		var prevBtn = $('#prevBtn');
@@ -89,16 +89,16 @@
 		var curUrlInfo = tabSource[tabIndex].list[listIndex].split('$');
 		// 播放
 		if(tabSource[tabIndex].type === 'player'){
-			playerBox.html(`<iframe webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" style="height: 100%; border: none;width: 100%;" class="player-frame" src="/player/index.html?${curUrlInfo[1]}" />`);
+			playerBox.html(`<iframe webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" style="height: ${frameH};border: none;width: 100%;background: #343434;" class="player-frame" src="/player/index.html?${curUrlInfo[1]}" />`);
 		}else if(tabSource[tabIndex].type === 'iframe'){
-			playerBox.html(`<iframe webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" style="height: 100%; border: none;width: 100%;" class="player-frame" src="${curUrlInfo[1]}" />`);
+			playerBox.html(`<iframe webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" style="height: ${frameH};border: none;width: 100%;background: #343434;" class="player-frame" src="${curUrlInfo[1]}" />`);
 		}
 		// 历史
 		setHistory(curUrlInfo[0]);
 		// 窗口变化
 		$(window).resize(function(){
 			var frameH = ($(window).width() < 990 ? 300 : 500) + 'px';
-			playerBox.css({height: frameH});
+			$('.player-frame').css({height: frameH});
 		});
 	}
 
